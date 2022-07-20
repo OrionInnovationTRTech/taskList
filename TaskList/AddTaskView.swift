50bec4cf96d5ced3d723c77deb0833ac46f56399
Seyit Murat Kaya <murat.kaya1269@gmail.com>
2022-07-20 13:13:19 +0300

    Add task creation and deletion

---
+++ TaskList/AddTaskView.swift
@@ -0,0 +1,31 @@
+//
+//  addTaskView.swift
+//  TaskList
+//
+//  Created by Seyit Murat Kaya on 20.07.2022.
+//
+
+import SwiftUI
+
+struct addTaskView: View {
+    
+    @Binding var title: String
+    @Binding var detail: String
+    
+    var body: some View {
+        Form{
+            Section(header:Text("Title")){
+                TextField("Enter your task", text:$title)
+            }
+            Section(header: Text("Details")){
+                TextEditor(text:$detail)
+            }
+        }
+    }
+}
+
+//struct addTaskView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        addTaskView()
+//    }
+//}
--- TaskList/ContentView.swift
+++ TaskList/ContentView.swift
@@ -11,25 +11,59 @@ struct ContentView: View {
     
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var tasks: FetchedResults<Tasks>
+    @State private var isAddingNewTask = false
+    @State var title: String = ""
+    @State var detail: String = ""
     
     var body: some View {
         List{
             ForEach(tasks){ task in
-                Text("deneme")
+                VStack(alignment: .leading){
+                    Text(task.title ?? "")
+                        .font(.headline)
+                    HStack{
+                        Text(task.creationDate! ,style: .date)
+                        Text(task.detail?.prefix(10) ?? "")
+                            + Text("...")
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                }
             }
             .onDelete(perform: deleteTask)
         }
         .navigationTitle("Tasks")
         .toolbar{
             Button {
-                let task = Tasks(context: moc)
-                task.id = UUID()
-                task.title = "title"
-                task.detail = "detail"
-                task.creationDate = Date.now
-                try? moc.save()
+                isAddingNewTask = true
             } label: {
                 Image(systemName: "plus")
+            }
+        }
+        .sheet(isPresented: $isAddingNewTask){
+            NavigationView{
+                addTaskView(title: $title, detail: $detail)
+                    .navigationTitle("Add New Task")
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Dismiss") {
+                                isAddingNewTask = false
+                                title = ""
+                                detail = ""
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Add") {
+                                let task = Tasks(context: moc)
+                                task.id = UUID()
+                                task.title = title
+                                task.detail = detail
+                                task.creationDate = Date.now
+                                isAddingNewTask = false
+                                try? moc.save()
+                            }
+                        }
+                    }
             }
         }
     }
